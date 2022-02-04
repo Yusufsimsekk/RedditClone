7106ebda8552bf70cb6da5f4dc35522b98067483
yusuf <mu.yusuf.99@gmail.com>
2022-02-04 15:15:21 +0300

    Initial Commit

---
+++ RedditClone/RedditCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedditCloneApp.swift
+//  RedditClone
+//
+//  Created by Yusuf Şimşek on 4.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct RedditCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
