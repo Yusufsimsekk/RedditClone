8d66316f4388fbf0b2ef057f109777d5179bf7d5
yusuf <mu.yusuf.99@gmail.com>
2022-02-05 10:22:00 +0300

    url and permalink update

--- RedditClone/Views/ContentView.swift
+++ RedditClone/Views/ContentView.swift
@@ -4,6 +4,7 @@ import SwiftUI
 
 struct ContentView: View {
     
+    var contentUrl = "https://www.reddit.com"
     @ObservedObject var networkManager = NetworkManager()
     
     var body: some View {
@@ -13,7 +14,7 @@ struct ContentView: View {
                 NavigationView {
                     List(networkManager.posts) { post in
                         
-                        NavigationLink(destination: DetailView(url: post.url)) {
+                        NavigationLink(destination: DetailView(url: contentUrl + post.permalink)) {
                             HStack {
                                 Text(String(post.ups))
                                 VStack {
@@ -24,8 +25,11 @@ struct ContentView: View {
                                         
                                     AsyncImage(url: URL(string: post.thumbnail)){ image in
                                         image.resizable()
+                                            .aspectRatio(contentMode: .fill)
                                             .frame(width: .infinity, height: 90)
+                                        
                                     } placeholder: {
+                                        
                                         Color.clear
                                             .frame(width: 0, height: 0)
                                     }
